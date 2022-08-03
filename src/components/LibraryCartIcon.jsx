3fd72e5652fcae07754fde4874566abd9368f11e
Romie Bett <bett@Betts-MacBook-Air.local>
2022-08-03 13:34:02 +0300

    cart feature added

--- src/components/LibraryCartIcon.jsx
+++ src/components/LibraryCartIcon.jsx
@@ -1,15 +1,34 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import CartContext from "../store/cart-context";
 import './headerbutton.css';
 
 const HeaderCartButton = props=>{
+    const [btnIsClicked, setBtnIsClicked] = useState(false);
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
+    const {items} = cartCtx;
+
+    const numberOfCartItems = items.reduce((curNumber, item)=>{
         return curNumber + item.amount;
-    },0)
-     
-    return <button onClick={props.onShowCart} className='button'>
+    },0);
+    const btnClasses = `button ${btnIsClicked ? `bump`: ''}`
+     useEffect(()=>{
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsClicked(true);
+
+        const timer = setTimeout(()=>{
+            setBtnIsClicked(false);
+        }, 300);
+        
+        return ()=>{
+            clearTimeout(timer);
+        };
+
+     }, [items]);  
+
+    return <button onClick={props.onShowCart} className={btnClasses}>
         <span className='icon'>
         <ShoppingCartOutlinedIcon />
         </span>
--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,22 +1,45 @@
-import React from "react"
+import React, {useContext} from "react"
 import './cartitems.css';
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-     const cartItems =<ul className="cart-items">{[{id:'c1', name:'part1', amount:2, price:12}
-    ].map((item)=><li>{item.name}</li>)}</ul>; 
+    const cartCtx = useContext(CartContext);
+    const totalAmount=`Ksh${cartCtx.totalAmount.toFixed(0)}`;
+    const hasItems = cartCtx.items.length>0;
+    
+    const cartItemRemoveHandler = (id)=>{
+        cartCtx.removeItem(id);
+    };
+    
+    const cartItemAddHandler = (item)=>{
+        cartCtx.addItem({...item, amount:1});
+    };
+
+     const cartItems =<ul className="cart-items">{
+       cartCtx.items.map((item)=>(
+        <CartItem 
+        key={item.id} 
+        name={item.name}
+        amount={item.amount} 
+        price={item.price} 
+        onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+        onAdd={cartItemAddHandler.bind(null, item)}/>
+        ))}</ul>
 
     return(
     <Modal onClose={props.onClose}>
         {cartItems}
-        
         <div className="total">
         <span> Total Amount</span>
-        <span>Ksh 90,000</span>
+        <span>{totalAmount}</span>
         </div>
         <div className="actions">
          <button onClick={props.onClose} className="button--alt">Close</button>
+         {hasItems && 
          <button  className="button">Order</button>
+        }
 
         </div>
     </Modal>
